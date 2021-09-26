3e92c302f1e5abd60817c3920ee0e624b847ae2d
Chris <chris.barwick16@gmail.com>
2021-09-26 15:54:27 -0700

    Fixed exports.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import type { ClientEvent } from "./client/ClientEvent";
-import type { WrappedClient } from "./client/WrappedClient";
-import type { ButtonCommand } from "./command/interaction/ButtonCommand";
-import type { ContextMenuCommand } from "./command/interaction/ContextMenuCommand";
-import type { SelectMenuCommand } from "./command/interaction/SelectMenuCommand";
-import type { MessageCommand } from "./command/MessageCommand";
-import type { SlashCommand } from "./command/slash/SlashCommand";
-import type { SlashCommandOption } from "./command/slash/SlashCommandOption";
-import type { SubCommand } from "./command/slash/SubCommand";
-import type { SubCommandGroup } from "./command/slash/SubCommandGroup";
+import { ClientEvent } from "./client/ClientEvent";
+import { WrappedClient } from "./client/WrappedClient";
+import { ButtonCommand } from "./command/interaction/ButtonCommand";
+import { ContextMenuCommand } from "./command/interaction/ContextMenuCommand";
+import { SelectMenuCommand } from "./command/interaction/SelectMenuCommand";
+import { MessageCommand } from "./command/MessageCommand";
+import { SlashCommand } from "./command/slash/SlashCommand";
+import { SlashCommandOption } from "./command/slash/SlashCommandOption";
+import { SubCommand } from "./command/slash/SubCommand";
+import { SubCommandGroup } from "./command/slash/SubCommandGroup";
 
 export * from "./types";
 export { WrappedClient, ClientEvent };
